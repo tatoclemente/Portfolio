295c55ce71c193ba7ec5a2935168a00bfaa00887
tatoclemente <tatoapuros@gmail.com>
2023-09-21 20:31:38 -0300

    Modal Contact 70%

--- app/[locale]/components/ContactModal/ContactModal.jsx
+++ app/[locale]/components/ContactModal/ContactModal.jsx
@@ -1,9 +1,18 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './ContactModal.module.css'
 import { ContactUs } from '../Form/ContactUs'
+import MsgContact from './MsgContact/MsgContact'
 const ContactModal = ( { contactIntl, contactClick } ) => {
+
+  const [showMsg, setShowMsg] = useState(true)
     document.body.classList.add(style.modalOpen);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setShowMsg(false)
+      }, 3000)
+    },[])
 
     useEffect(() => {
         return () => {
@@ -12,10 +21,16 @@ const ContactModal = ( { contactIntl, contactClick } ) => {
       }, []);
   return (
     <div className={style.modalContainer}>
-        <div className={style.modal}>
-            <span onClick={contactClick}>X</span>
-        <ContactUs contactIntl={contactIntl} />
-        </div>
+      <div className={style.modal}>
+        {
+          showMsg 
+          ? <MsgContact />
+          : <div className={style.modalContent}>
+              <span className={style.close} onClick={contactClick}>X</span>
+              <ContactUs contactIntl={contactIntl} />
+            </div>
+        }
+      </div>
     </div>
   )
 }
---
+++ app/[locale]/components/ContactModal/MsgContact/MsgContact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from './MsgContact.module.css'
+const MsgContact = () => {
+  return (
+    <div className={style.msgContact}>
+      <div className={style.titleContainer}>
+        <h1>¡Me agrada tu actitud...</h1>
+        <h1>...eres una persona decidida!</h1>
+      </div>
+      <h3>Por eso vamos directo allí...</h3>
+  
+    </div>
+  )
+}
+
+export default MsgContact
