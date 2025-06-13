3861b8bd7531e60207be0761d9d239bcc78875e8
Gustavo <soytatoclemente@gmail.com>
2025-06-13 13:31:14 -0300

    Feat: Loading spinner CV

--- app/[locale]/components/DownloadResume/modalPdf.jsx
+++ app/[locale]/components/DownloadResume/modalPdf.jsx
@@ -1,7 +1,25 @@
+'use client'
 import styles from './modalPdf.module.css'
+import { useEffect, useState } from 'react';
 
 const ModalPDF = ({ isOpenPdf, pdfLink, closePdfModal }) => {
   if (!isOpenPdf || !pdfLink) return null
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [loading, setLoading] = useState(true);
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    if (isOpenPdf && pdfLink) {
+      setLoading(true);
+    }
+  }, [isOpenPdf, pdfLink]);
+
+  if (!isOpenPdf || !pdfLink) return null;
+
+  const handleIframeLoad = () => {
+    setLoading(false);
+  };
+
 
   return (
     <div className={styles.overlay}>
@@ -9,10 +27,17 @@ const ModalPDF = ({ isOpenPdf, pdfLink, closePdfModal }) => {
         <button className={styles.closeButton} onClick={closePdfModal}>
           Cerrar
         </button>
+           {loading && (
+          <div className={styles.spinnerContainer}>
+            <div className={styles.spinner}></div>
+          </div>
+        )}
         <iframe 
           src={pdfLink} 
           className={styles.iframeViewer} 
           title="Visor de PDF" 
+          onLoad={handleIframeLoad}
+          style={loading ? { visibility: 'hidden' } : { visibility: 'visible' }}
         />
       </div>
     </div>
