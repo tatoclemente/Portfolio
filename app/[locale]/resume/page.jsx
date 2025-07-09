eba6fb887a889151d078ce3eafe2e5cb63e2b63f
Gustavo <soytatoclemente@gmail.com>
2025-07-09 01:11:31 -0300

    Fix: :styles: Porfolio button back

--- app/[locale]/resume/page.jsx
+++ app/[locale]/resume/page.jsx
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import Resume from '../components/Resume/resume';
 import styles from './resume.module.css';
+import { BiArrowBack } from 'react-icons/bi';
 
 const ResumePage = () => {
   const t = useTranslations('NavBar'); 
@@ -9,7 +10,7 @@ const ResumePage = () => {
     <div className={styles.mainContainer}>
       <div className={styles.resumeContainer}>
         <Link href={'/'} className={styles.backButton}>
-          { t('BackToPorfolio') }
+          <BiArrowBack /> { t('BackToPorfolio') }
         </Link>
         <Resume />
       </div>
