1986550ee73271f1b98ce401837bcf824585e998
Gustavo <soytatoclemente@gmail.com>
2025-07-09 01:06:26 -0300

    Update: Add /resume into sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,12 +6,12 @@ export default function sitemap() {
       changeFrequency: 'monthly',
       priority: 1,
     },
-    // {
-    //   url: 'https://tatoclemente.dev/projects',
-    //   lastModified: new Date(),
-    //   changeFrequency: 'weekly',
-    //   priority: 0.8,
-    // },
+    {
+      url: 'https://tatoclemente.dev/resume',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
     // {
     //   url: 'https://tatoclemente.dev/about',
     //   lastModified: new Date(),
