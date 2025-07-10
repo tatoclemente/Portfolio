d92beab94ee076dec09f944c92b2ff872ba70071
Gustavo <soytatoclemente@gmail.com>
2025-07-10 17:44:00 -0300

    Improvment: Add dpwnload Cv button

--- app/[locale]/resume/page.jsx
+++ app/[locale]/resume/page.jsx
@@ -1,17 +1,22 @@
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import Resume from '../components/Resume/resume';
-import styles from './resume.module.css';
-import { BiArrowBack } from 'react-icons/bi';
+import styles from './resume-page.module.css';
+import { BiArrowBack, BiDownload } from 'react-icons/bi';
 
 const ResumePage = () => {
   const t = useTranslations('NavBar'); 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.resumeContainer}>
-        <Link href={'/'} className={styles.backButton}>
-          <BiArrowBack /> { t('BackToPorfolio') }
-        </Link>
+        <div className={styles.actionsContainer}>
+          <Link href={'/'} className={styles.backButton}>
+            <BiArrowBack /> { t('BackToPorfolio') }
+          </Link>
+          <a className={styles.downloadButton} href="/cv/Resume_Gustavo_Clemente_Dev_Frontend.pdf" download>
+            <BiDownload /> { t('DownloadResume') }
+          </a>
+        </div>
         <Resume />
       </div>
     </div>
