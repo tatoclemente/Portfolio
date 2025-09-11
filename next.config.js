6bcb43f61372aa9acf1b35020e4f01d934ab3073
Gustavo <soytatoclemente@gmail.com>
2025-09-11 20:17:47 -0300

    Fix: update cache control /resume route

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/resume',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
+  },
+}
 
 const withNextIntl = require('next-intl/plugin')(
     // This is the default (also the `src` folder is supported out of the box)
